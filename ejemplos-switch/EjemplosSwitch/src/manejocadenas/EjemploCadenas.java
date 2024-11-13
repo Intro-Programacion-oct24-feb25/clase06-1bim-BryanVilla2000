058dcb2c1de1c552da4202bf4dcf38eb54e7b207
Villa <bryan.m.villa.c@gmail.com>
2024-11-13 12:30:56 -0500

    Comentario

--- ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
+++ ejemplos-switch/EjemplosSwitch/src/manejocadenas/EjemploCadenas.java
@@ -10,14 +10,21 @@ package manejocadenas;
  * @author reroes
  */
 public class EjemploCadenas {
+
     public static void main(String[] args) {
         String ciudad = "loJa";
+        /*
+        Usamos el toUpperCase para poner todo en mayuscula y el toLowerCase
+        para poner todo en minuscula
+        */
         ciudad = ciudad.toUpperCase();
+        // se presentqa loja en minuscula porue solo estamos tranformando para 
+        //la presentacion
         System.out.printf("%s\n", ciudad.toLowerCase());
         // System.out.printf("%s\n", ciudad.toUpperCase());
         System.out.printf("%s\n", ciudad);
         // toLowerCase es un método
         // toUpperCase es un método
-        
+
     }
 }
